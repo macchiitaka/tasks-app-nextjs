0a2bafb91f996c13020c888ab9d99633535b4e81
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2022-03-05 17:27:56 +0900

    test(unit): fix

--- src/interfaces/ui/components/TaskList/component.stories.tsx
+++ src/interfaces/ui/components/TaskList/component.stories.tsx
@@ -41,9 +41,27 @@ ClickFirstCheckbox.parameters = {
 ClickFirstCheckbox.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
   const checkbox = await canvas.findByRole('checkbox', { name: 'TODO_01' });
+  await userEvent.click(checkbox);
+};
+
+export const TestToggleFirstCheckbox = Template.bind({});
+TestToggleFirstCheckbox.parameters = {
+  msw: {
+    handlers: [getFetchTasksHandlers()],
+  },
+};
+TestToggleFirstCheckbox.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const checkbox = await canvas.findByRole('checkbox', { name: 'TODO_01' });
   expect(checkbox).not.toBeChecked();
+
   await userEvent.click(checkbox);
   await waitFor(async () => {
     await expect(checkbox).toBeChecked();
+  });
+
+  await userEvent.click(checkbox);
+  await waitFor(async () => {
+    await expect(checkbox).not.toBeChecked();
   });
 };
